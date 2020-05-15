844368f4a65d54c105b53fb0533fa38eaadb1313
HDLY <42459504+HandeulLy@users.noreply.github.com>
2020-05-15 09:13:55 +0900

    프로그래머스, 가운데 글자 가져오기

--- Programmers/가운데글자가져오기.py
+++ Programmers/가운데글자가져오기.py
@@ -1,6 +1,6 @@
 def solution(s):
     answer = ''
-    size = len(s) # 문자열의 길이를 저장
+    size = len(s) # 문자열 길이 저장
 
     if size%2 == 0 : # 단어의 길이가 짝수라면 가운데 두글자
         answer = s[int(size/2-1):int(size/2+1)]
