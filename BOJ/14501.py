ad199ffe65852471934341fc157116f24ce6d95f
HDLY <42459504+HandeulLy@users.noreply.github.com>
2020-05-16 16:37:49 +0900

    백준, 14501, 퇴사

--- BOJ/14501.py
+++ BOJ/14501.py
@@ -9,7 +9,6 @@ def solve(day, pay) :
         return
     elif day > N : # 퇴사 후 상담은 못하니까 연산 종료
         return
-
     solve(day + tasks[day][0], pay + tasks[day][1]) # 해당 일의 상담을 시작하면 다음 상담은 지금 상담 끝난 후
     solve(day + 1, pay) # 해당 일 상담 안하고 다음 날 상담
 
