79c8d888eded1a3a28f0b3245fe86087b60ff421
HDLY <42459504+HandeulLy@users.noreply.github.com>
2020-06-18 22:08:03 +0900

    프로그래머스, 짝수와 홀수

---
+++ Programmers/짝수와홀수.py
@@ -0,0 +1,11 @@
+# Solution 1
+def solution1(num):
+    if num%2==0 : return "Even"
+    else : return "Odd"
+
+############
+
+# Solution 2
+
+def solution2(num):
+    return "Even" if num%2 == 0 else "Odd"
